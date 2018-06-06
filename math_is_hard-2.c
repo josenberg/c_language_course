fe168ceae26956e221f1e3cbfc3c8e4846a04178
Josenberg <mbx.philipe@gmail.com>
2018-06-05 23:05:45 -0300

    math_is_hard

---
+++ math_is_hard-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  float a;
+  a = 5.0 / 3.0;
+  printf("%f\n", a);
+}
---
+++ math_is_hard.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  float a;
+  a = 5 / 3;
+  printf("%f\n", a);
+}
