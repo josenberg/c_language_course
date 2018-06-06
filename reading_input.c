7e3757b3c38e99b777feac731ecd674023c271a6
Jose Philipe Mendes de Godoy <ra200073@setclone.bash.lab.ic.unicamp.br>
2018-06-06 18:35:01 -0300

    reading input from the suer

---
+++ reading_input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int var;
+  printf("Insira a nota do minicurso de C (dica: 10) \n");
+  scanf("%d", &var);
+  printf("A nota que voce escolheu foi: %d \n", var);
+}
